c05b8d87948db98005fb1f95fd3fc6ef9e8ee82c
Robert12s <roberttwelves@exe-coll.ac.uk>
2025-02-24 20:48:50 +0000

    ccs adjust

--- templates/page/header.tpl
+++ templates/page/header.tpl
@@ -1,6 +1,6 @@
 <header>
     <nav>
-        <a href="/topics.php">Topics Page</a>
+        <a class="bbtitle" href="/topics.php">Topics Page</a>
         <a href="/login.php">
             <img src="/assets/img/person-circle.svg" height="50" width="50" alt="account picture">
         </a>
--- templates_c/79813c99a7b5ab906c9ab5f2d853fe25423f9372_0.file_header.tpl.php
+++ templates_c/79813c99a7b5ab906c9ab5f2d853fe25423f9372_0.file_header.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.4.3, created on 2025-02-24 20:25:28
+/* Smarty version 5.4.3, created on 2025-02-24 20:47:37
   from 'file:page/header.tpl' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.4.3',
-  'unifunc' => 'content_67bcd5b8efd941_87467360',
+  'unifunc' => 'content_67bcdae904b8f2_11466749',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '79813c99a7b5ab906c9ab5f2d853fe25423f9372' => 
     array (
       0 => 'page/header.tpl',
-      1 => 1740427512,
+      1 => 1740430055,
       2 => 'file',
     ),
   ),
@@ -20,11 +20,11 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_67bcd5b8efd941_87467360 (\Smarty\Template $_smarty_tpl) {
+function content_67bcdae904b8f2_11466749 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'C:\\repos\\HND_Agile25\\templates\\page';
 ?><header>
     <nav>
-        <a href="/topics.php">Topics Page</a>
+        <a class="bbtitle" href="/topics.php">Topics Page</a>
         <a href="/login.php">
             <img src="/assets/img/person-circle.svg" height="50" width="50" alt="account picture">
         </a>
